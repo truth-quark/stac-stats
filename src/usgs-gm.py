c6a857ec25f1b7be88aa52e62285c0f70840786d
Imam Alam <imam.alam@gmail.com>
2025-10-16 00:38:02 +0000

    cleanup

--- src/usgs-gm.py
+++ src/usgs-gm.py
@@ -1,19 +1,16 @@
-# run this with GASuperUser credentials
-# also install zarr?
-"Special thanks to Chad Barton"
+"""Special thanks to Chad Barton for the PoC"""
+
+from datetime import datetime
 import numpy
-import xarray
 from pystac_client import Client
 from dask import array as da
 import dask.distributed
-from datetime import datetime
 
-from odc.geo import BoundingBox
-from odc.geo.xr import write_cog
-from odc.stac import configure_rio, stac_load
 from odc.algo import xr_geomedian
-
+from odc.geo import BoundingBox
+from odc.geo.xr import write_cog, assign_crs
 from odc.io.cgroups import get_cpu_quota
+from odc.stac import configure_rio, stac_load
 
 
 def rewrite_asset_urls(in_url):
@@ -23,32 +20,34 @@ def rewrite_asset_urls(in_url):
         return in_url
     return s3_prefix + in_url[len(http_prefix):]
 
-start_date = "2023-01-01"
-end_date = "2023-12-31"
+year = 2023
 
-crs="EPSG:4326"
-central_lat =  -35.0
-central_lon =  150.0
-buffer = 0.1
-latitude = (central_lat - buffer, central_lat + buffer)
-longitude = (central_lon - buffer, central_lon + buffer)
+query_crs="EPSG:4326"
+output_crs = "EPSG:3577"
 
 measurements = ['coastal', 'blue', 'green', 'red', 'nir08', 'swir16', 'swir22']
 masking_band = "qa_pixel"
+
+central_lat =  -35.0
+central_lon =  150.0
+buffer = 0.1
 
 bbox = BoundingBox(
     left=central_lon - buffer,
     bottom=central_lat - buffer,
     right=central_lon + buffer,
     top=central_lat + buffer,
-    crs=crs
+    crs=query_crs
 )
 
 
-def search(stac_client):
+def search():
+    stac_client = Client.open("https://landsatlook.usgs.gov/stac-server")
     l2col = 'landsat-c2l2-sr'
     collections_query = [l2col]
 
+    start_date = f"{year}-01-01"
+    end_date = f"{year}-12-31"
     date_query = f"{start_date}/{end_date}"
 
     return stac_client.search(
@@ -59,8 +58,6 @@ def search(stac_client):
 
 
 def load(items):
-    output_crs = "EPSG:3577"
-
     optical_ds = stac_load(
         items=items,
         bands=measurements,
@@ -101,32 +98,28 @@ def load(items):
 
     return optical_ds
 
-def write(ds):
-    for i, time in enumerate(ds['time']):
+def write_input_data(ds):
+    for i, _ in enumerate(ds['time']):
         for band in measurements + [masking_band]:
             write_cog(ds[band].isel(time=i), f'/output/{band}_{i}.tif', overwrite=True)
 
-    
-    # ds.to_zarr("s3://imam-dev-bucket/test1/test.zarr")
-
 def write_geomedian(gm):
     for band in measurements:
-       write_cog(gm[band], f'/output/{band}_gm.tif', overwrite=True) #.compute()
-    # gm.to_zarr("/output/gm.zarr", mode="w")
+       write_cog(gm[band], f'/output/{band}_gm_{year}.tif', overwrite=True)
 
-
-def main():
+def setup():
     dask_client = dask.distributed.Client(n_workers=1, threads_per_worker=get_cpu_quota())
     configure_rio(cloud_defaults=True, aws={"requester_pays": True}, client=dask_client)
 
-    stac_client = Client.open("https://landsatlook.usgs.gov/stac-server")
+def main():
+    setup()
 
     print('searching', datetime.now())
-    items = search(stac_client)
+    items = search()
     print('loading', datetime.now())
     ds = load(items).persist()
     print('geomedian', datetime.now())
-    gm = xr_geomedian(ds).load()
+    gm = assign_crs(xr_geomedian(ds).load(), crs=output_crs)
     print('writing', datetime.now())
     write_geomedian(gm)
     print('done', datetime.now())
