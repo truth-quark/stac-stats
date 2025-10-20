fccf101e79e20184c91b97f7a447de8703db2f3a
Imam Alam <imam.alam@gmail.com>
2025-10-20 10:00:15 +0000

    fix inputs

--- src/usgs_gm.py
+++ src/usgs_gm.py
@@ -34,12 +34,12 @@ s3_prefix = "usgs-gm/geomad"
 
 
 def read_tasks_list():
-    with open("inputs/tasks.list") as fl:
+    with open("/inputs/tasks.list") as fl:
         return [line.strip() for line in fl]
 
 
 def write_tasks_list(tasks_list):
-    with open("inputs/tasks.list", "w") as fl:
+    with open("/inputs/tasks.list", "w") as fl:
         for task in tasks_list:
             print(task, file=fl)
 
