9433edd0e25362f00f3470011114f40359d460b3
Imam Alam <imam.alam@gmail.com>
2026-08-18 11:41:24 +0000

    wrong nodata value for SR?

--- src/usgs_gm.py
+++ src/usgs_gm.py
@@ -22,7 +22,7 @@ from odc.stac import configure_rio, stac_load
 query_crs = "EPSG:4326"
 output_crs = "EPSG:32757"
 
-measurements = ['coastal', 'blue', 'green', 'red', 'nir08', 'swir16', 'swir22']
+measurements = ["coastal", "blue", "green", "red", "nir08", "swir16", "swir22"]
 masking_band = "qa_pixel"
 
 product = "2025-07-present"
@@ -36,6 +36,7 @@ class TaskMetaData(typing.NamedTuple):
 
     Dates have the form 'YYYY-MM-DD'
     """
+
     start_date: str
     end_date: str
 
@@ -57,30 +58,30 @@ def write_tasks_list(tasks_list):
 
 
 def rewrite_asset_urls(in_url):
-    http_prefix = 'https://landsatlook.usgs.gov/data/'
-    s3_prefix = 's3://usgs-landsat/'
+    http_prefix = "https://landsatlook.usgs.gov/data/"
+    s3_prefix = "s3://usgs-landsat/"
     if not in_url.startswith(http_prefix):
         return in_url
-    return s3_prefix + in_url[len(http_prefix):]
+    return s3_prefix + in_url[len(http_prefix) :]
 
 
 def extract_feature(region_code):
     with open("/src/gm_polygons.geojson") as fl:
         data = json.load(fl)
 
-    features = data['features']
+    features = data["features"]
 
     for feature in features:
-        if feature['properties']['region_code'] == region_code:
+        if feature["properties"]["region_code"] == region_code:
             return feature
 
     raise ValueError(f"region not found: {region_code}")
 
 
 def bounds(feature):
-    geom = feature['geometry']
-    assert geom['type'] == "Polygon"
-    coords = geom['coordinates']
+    geom = feature["geometry"]
+    assert geom["type"] == "Polygon"
+    coords = geom["coordinates"]
     assert len(coords) == 1
     points = coords[0]
     lons = [p[0] for p in points]
@@ -88,32 +89,40 @@ def bounds(feature):
     left, right = min(lons), max(lons)
     top, bottom = max(lats), min(lats)
 
-    return BoundingBox(
-        left=left, top=top, right=right, bottom=bottom, crs=query_crs
-    )
+    return BoundingBox(left=left, top=top, right=right, bottom=bottom, crs=query_crs)
 
 
 def search(bbox, meta: TaskMetaData):
     stac_client = Client.open("https://landsatlook.usgs.gov/stac-server")
-    l2col = 'landsat-c2l2-sr'
+    l2col = "landsat-c2l2-sr"
 
     date_query = f"{meta.start_date}/{meta.end_date}"
 
-    landsat8 = stac_client.search(
-        collections=[l2col],
-        datetime=date_query,
-        bbox=bbox.bbox,
-        query={"platform": {"eq": "LANDSAT_8"}}
-    ).item_collection().items
+    landsat8 = (
+        stac_client.search(
+            collections=[l2col],
+            datetime=date_query,
+            bbox=bbox.bbox,
+            query={"platform": {"eq": "LANDSAT_8"}},
+        )
+        .item_collection()
+        .items
+    )
 
-    landsat9 = stac_client.search(
-        collections=[l2col],
-        datetime=date_query,
-        bbox=bbox.bbox,
-        query={"platform": {"eq": "LANDSAT_9"}}
-    ).item_collection().items
+    landsat9 = (
+        stac_client.search(
+            collections=[l2col],
+            datetime=date_query,
+            bbox=bbox.bbox,
+            query={"platform": {"eq": "LANDSAT_9"}},
+        )
+        .item_collection()
+        .items
+    )
 
-    return ItemCollection(sorted(landsat8 + landsat9, key=lambda item: item.properties['datetime']))
+    return ItemCollection(
+        sorted(landsat8 + landsat9, key=lambda item: item.properties["datetime"])
+    )
 
 
 def load_mask(items, bbox):
@@ -148,40 +157,55 @@ def load_optical(items, bbox):
             patch_url=rewrite_asset_urls,
         )
 
-    nodata = 65535
+    nodata = 0
     scale = 0.00002750
     offset = -0.200000
     rescale = 10000.0
 
     for band in measurements:
-        optical_ds[band] = (optical_ds[band].dims, numpy.where(optical_ds[band].data != nodata, optical_ds[band].data, numpy.nan))
+        optical_ds[band] = (
+            optical_ds[band].dims,
+            numpy.where(
+                optical_ds[band].data != nodata, optical_ds[band].data, numpy.nan
+            ),
+        )
         optical_ds[band] = (optical_ds[band] * scale + offset) * rescale
-        optical_ds[band] = (optical_ds[band].dims, numpy.clip(optical_ds[band], 0, rescale).data)
+        optical_ds[band] = (
+            optical_ds[band].dims,
+            numpy.clip(optical_ds[band], 0, rescale).data,
+        )
     return optical_ds
 
 
 def load(items, bbox):
-    log('loading mask', datetime.now())
+    log("loading mask", datetime.now())
     mask = load_mask(items, bbox)
-    log('loading bands', datetime.now())
+    log("loading bands", datetime.now())
     optical_ds = load_optical(items, bbox)
 
-    log('masking', datetime.now())
+    log("masking", datetime.now())
     for band in measurements:
-        optical_ds[band] = (optical_ds[band].dims, numpy.where(mask, optical_ds[band], numpy.nan))
+        optical_ds[band] = (
+            optical_ds[band].dims,
+            numpy.where(mask, optical_ds[band], numpy.nan),
+        )
 
     return optical_ds
 
 
 def write_input_data(ds):
-    for i, time in enumerate(numpy.datetime_as_string(ds['time'].data)):
+    for i, time in enumerate(numpy.datetime_as_string(ds["time"].data)):
         for band in measurements:
-            write_cog(ds[band].isel(time=i).compute(), f'/output/{band}_{time}_{i}.tif', overwrite=True)
+            write_cog(
+                ds[band].isel(time=i).compute(),
+                f"/output/{band}_{time}_{i}.tif",
+                overwrite=True,
+            )
 
 
 def write_geomedian(gm, region_code, upload=False):
     if upload:
-        s3_client = boto3.client('s3')
+        s3_client = boto3.client("s3")
     else:
         s3_client = None
 
@@ -190,11 +214,11 @@ def write_geomedian(gm, region_code, upload=False):
     (root / folder).mkdir(parents=True, exist_ok=True)
 
     for band in measurements:
-       filename = f'{folder}/gm_{product}_{region_code}_{band}.tif'
-       on_disk = str(root / filename)
-       write_cog(gm[band], on_disk, overwrite=True)
-       if upload:
-           s3_client.upload_file(on_disk, s3_bucket, f"{s3_prefix}/{filename}")
+        filename = f"{folder}/gm_{product}_{region_code}_{band}.tif"
+        on_disk = str(root / filename)
+        write_cog(gm[band], on_disk, overwrite=True)
+        if upload:
+            s3_client.upload_file(on_disk, s3_bucket, f"{s3_prefix}/{filename}")
 
     filename = f"{folder}/gm_{product}_{region_code}.completed"
     on_disk = str(root / filename)
@@ -207,7 +231,7 @@ def write_geomedian(gm, region_code, upload=False):
 def check_exists(region_code):
     return False
 
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client("s3")
     folder = f"usgs_ls_gm/{region_code}"
     filename = f"{folder}/gm_{product}_{region_code}.completed"
     try:
@@ -221,26 +245,25 @@ def execute_task(region_code, meta: TaskMetaData):
     configure_rio(cloud_defaults=True, aws={"requester_pays": True})
 
     bbox = bounds(extract_feature(region_code))
-    log('searching', bbox.bbox, region_code, datetime.now())
+    log("searching", bbox.bbox, region_code, datetime.now())
     items = search(bbox, meta)
-    log('loading', datetime.now())
+    log("loading", datetime.now())
     ds = load(items, bbox)
     # log('writing input', datetime.now())
     # write_input_data(ds)
-    log('geomedian', datetime.now())
-    gm = assign_crs(xr_geomedian(ds, num_threads=multiprocessing.cpu_count()), crs=output_crs)
-    log('writing', datetime.now())
+    log("geomedian", datetime.now())
+    gm = assign_crs(
+        xr_geomedian(ds, num_threads=multiprocessing.cpu_count()), crs=output_crs
+    )
+    log("writing", datetime.now())
     write_geomedian(gm, region_code)
 
-    log('done', datetime.now())
+    log("done", datetime.now())
 
 
 def main():
     # TODO: gather date strings & job specific params here as needed
-    meta = TaskMetaData(
-        start_date="2025-07-01",
-        end_date="2026-12-31"
-    )
+    meta = TaskMetaData(start_date="2025-07-01", end_date="2026-12-31")
 
     tasks_list = read_tasks_list()
 
@@ -250,10 +273,11 @@ def main():
         if not check_exists(region_code):
             execute_task(region_code, meta)
         else:
-            log(region_code, 'already exists!')
+            log(region_code, "already exists!")
 
         tasks_list.remove(region_code)
         write_tasks_list(tasks_list)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
